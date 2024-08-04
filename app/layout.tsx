d0ef8050129af78b4e4b9e851558ccea791efbf0
Gonzalo Chale <miguelgchale@gmail.com>
2024-08-04 00:01:38 -0500

    chore:setup plausible analitycs

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/sonner";
 import { nanoid } from "@/lib/utils";
 import { AI } from "@/lib/chat/actions";
+import PlausibleProvider from "next-plausible";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -24,9 +25,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   const id = nanoid();
+  const domain = process.env.NEXT_PUBLIC_DOMAIN as string;
+  const customDomain = process.env
+    .NEXT_PUBLIC_PLAUSIBLE_CUSTOM_DOMAIN as string;
   return (
     <AI initialAIState={{ chatId: id, messages: [] }}>
       <html lang="en">
+        <head>
+          <PlausibleProvider domain={domain} customDomain={customDomain} />
+        </head>
         <body
           className={cn(
             "min-h-screen bg-background antialiased",
